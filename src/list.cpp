451d0ea3acb65cff17279dd31298a6d8a6143012
GadeevDmitry <gadeev.dima@mail.ru>
2022-10-23 20:54:58 +0300

    Last list version1

--- src/list.cpp
+++ src/list.cpp
@@ -36,7 +36,7 @@
 #define HASH_DATA_PROTECTION        0
 #endif
 
-#if     CANARY_LIST_PROTECTION || CANARY_DATA_PROTECTION || HASH_PROTECTION
+#if     CANARY_LIST_PROTECTION || CANARY_DATA_PROTECTION || HASH_LIST_PROTECTION || HASH_DATA_PROTECTION
 #define PROTECTION                  1
 #else
 #define PROTECTION                  0
@@ -47,7 +47,6 @@ struct List_elem_info
     bool is_free;
 
     void *prev;
-    void *cur;
     void *next;
 };
 
@@ -91,40 +90,45 @@ struct List
     #endif
 };
 
+enum POISON
+{
+    POISON_ELEM_BYTE = (uint32_t) 345
+};
+
 enum LIST_VERDICT
 {
-    OK                                              ,
+    OK                                         = 0   ,
 
 #if CANARY_LIST_PROTECTION
-    FAILED_LIST_CANARY_PROTECTION_LEFT              ,
-    FAILED_LIST_CANARY_PROTECTION_RIGHT             ,
+    FAILED_LIST_CANARY_PROTECTION_LEFT   = (1 << 0)  ,
+    FAILED_LIST_CANARY_PROTECTION_RIGHT  = (1 << 1)  ,
 #endif
 
 #if CANARY_DATA_PROTECTION
-    FAILED_DATA_CANARY_PROTECTION_LEFT              ,
-    FAILED_DATA_CANARY_PROTECTION_RIGHT             ,
+    FAILED_DATA_CANARY_PROTECTION_LEFT   = (1 << 2)  ,
+    FAILED_DATA_CANARY_PROTECTION_RIGHT  = (1 << 3)  ,
 #endif
 
 #if HASH_LIST_PROTECTION
-    FAILED_LIST_HASH_PROTECTION                     ,
+    FAILED_LIST_HASH_PROTECTION          = (1 << 4)  ,
 #endif
 
 #if HASH_DATA_PROTECTION
-    FAILED_DATA_HASH_PROTECTION                     ,
+    FAILED_DATA_HASH_PROTECTION          = (1 << 5)  ,
 #endif
 
-    NEGATIVE_SIZE                                   ,
-    NEGATIVE_CAPACITY                               ,
-    NEGATIVE_ELEM_SIZE                              ,
-    NEGATIVE_FIRST_FREE                             ,
+    NEGATIVE_SIZE                        = (1 << 6)  ,
+    NEGATIVE_CAPACITY                    = (1 << 7)  ,
+    NEGATIVE_ELEM_SIZE                   = (1 << 8)  ,
+    NEGATIVE_FIRST_FREE                  = (1 << 9)  ,
 
-    INVALID_CAPACITY                                ,
-    INVALID_FIRST_FREE                              ,
-    INVALID_DATA                                    ,
-    INVALID_CTOR                                    ,
-    INVALID_MEMORY                                  ,
+    INVALID_CAPACITY                     = (1 << 10) ,
+    INVALID_FIRST_FREE                   = (1 << 11) ,
+    INVALID_DATA                         = (1 << 12) ,
+    INVALID_CTOR                         = (1 << 13) ,
+    INVALID_MEMORY                       = (1 << 14) ,
 
-    LIST_NULLPTR
+    LIST_NULLPTR                         = (1 << 15)
 };
 
 static const char *verdict_message[] = 
@@ -191,26 +195,36 @@ enum CANARY_VALUE
 
 #define log_place_dump() log_message("%20s, %35s, %6d\n", call_file, call_func, call_line)
 
+#define List_realloc(lst)                                                                                   \
+       _List_realloc(lst, __FILE__, __PRETTY_FUNCTION__, __LINE__)
+
+#define List_fill_free(lst)                                                                                 \
+       _List_fill_free(lst, __FILE__, __PRETTY_FUNCTION__, __LINE__)
+
 #if PROTECTION
 
-static uint32_t  _List_ctor     (List *const lst, const uint32_t elem_size, const char    *name_file,
-                                                                            const char    *name_func,
-                                                                            const char    *name_var ,
-                                                                            const uint32_t line);
+static  int32_t  _List_ctor         (List *const lst, const uint32_t elem_size, const char    *name_file,
+                                                                                const char    *name_func,
+                                                                                const char    *name_var ,
+                                                                                const uint32_t line);
 #else //!PROTECTION
 
-static uint32_t  _List_ctor     (List *const lst, const uint32_t elem_size, const char *call_file,
-                                                                            const char *call_func,
-                                                                            const char *call_line);
+static  int32_t  _List_ctor         (List *const lst, const uint32_t elem_size, const char *call_file,
+                                                                                const char *call_func,
+                                                                                const char *call_line);
 #endif
 
-/*___________________________MAIN_FUNCTION_DECLARATION___________________________*/
+static int8_t    _List_fill_free    (List *const lst,   const char *call_file,
+                                                        const char *call_func,
+                                                        const char *call_line);
+
+/*___________________________MAIN_FUNCTIONS_DECLARATIONS__________________________*/
 
 static uint32_t  _List_verify   (List *const lst);
 
 #if PROTECTION
 
-static uint32_t __List_ctor     (List *const lst, const uint32_t elem_size, const char    *name_file,
+static  int32_t __List_ctor     (List *const lst, const uint32_t elem_size, const char    *name_file,
                                                                             const char    *name_func,
                                                                             const char    *name_var ,
                                                                             const uint32_t line);
@@ -252,10 +266,11 @@ static uint32_t _List_verify(List *const lst)
     if (lst->first_free    < 0)                             err = err | NEGATIVE_FIRST_FREE                 ;
 
     if (lst->data_capacity < lst->data_size)                err = err | INVALID_CAPACITY                    ;
-    if (lst->first_free   >= lst->data_capacity)            err = err | INVALID_FIRST_FREE                  ;
+    if (lst->first_free    > lst->data_capacity)            err = err | INVALID_FIRST_FREE                  ;
 
     if (lst->data == nullptr && lst->data_size)             err = err | INVALID_DATA                        ;
 
+    #if HASH_DATA_PROTECTION
     uint32_t hash_data_check =  NEGATIVE_SIZE       |
                                 NEGATIVE_CAPACITY   |
                                 NEGATIVE_ELEM_SIZE  |
@@ -264,23 +279,26 @@ static uint32_t _List_verify(List *const lst)
     
     if (!(err & hash_data_check) &&
         !hash_verify(lst, &lst->hash_data, lst->data_size)) err = err | FAILED_DATA_HASH_PROTECTION         ;
+    #endif
 
-    return err; 
+    return err;
 }
+
+/*_______________________________________________________________________________*/
 
 #if PROTECTION
 
-static uint32_t  _List_ctor(List *const lst, const uint32_t elem_size,  const char    *name_file,
+static  int32_t  _List_ctor(List *const lst, const uint32_t elem_size,  const char    *name_file,
                                                                         const char    *name_func,
                                                                         const char    *name_var ,
                                                                         const uint32_t line)
 {
-    uint32_t ret_val  =     __List_ctor(lst, elem_size,      name_file, name_func, name_var, line);
-    if      (ret_val != OK) log_message("%20s, %35s, %6d\n", name_file, name_func,           line);
-    return   ret_val;
+    int32_t ret_val  =     __List_ctor(lst, elem_size,      name_file, name_func, name_var, line);
+    if     (ret_val != OK) log_message("%20s, %35s, %6d\n", name_file, name_func,           line);
+    return  ret_val;
 }
 
-static uint32_t __List_ctor(List *const lst, const uint32_t elem_size,  const char    *name_file,
+static  int32_t __List_ctor(List *const lst, const uint32_t elem_size,  const char    *name_file,
                                                                         const char    *name_func,
                                                                         const char    *name_var ,
                                                                         const uint32_t line)
@@ -312,16 +330,16 @@ static uint32_t __List_ctor(List *const lst, const uint32_t elem_size,  const ch
 #define List_ctor(lst, elem_size)                                                                           \
        _List_ctor(lst, elem_size, __FILE__, __PRETTY_FUNCTION__, __LINE__)
 
-static uint32_t  _List_ctor(List *const lst, const uint32_t elem_size,  const char *call_file,
+static  int32_t  _List_ctor(List *const lst, const uint32_t elem_size,  const char *call_file,
                                                                         const char *call_func,
                                                                         const char *call_line)
 {
-    uint32_t ret_val  =     __List_ctor   (lst, elem_size);
-    if      (ret_val != OK) log_place_dump()              ;
-    return   ret_val;
+    int32_t ret_val  =     __List_ctor   (lst, elem_size);
+    if     (ret_val != OK) log_place_dump()              ;
+    return  ret_val;
 }
 
-static uint32_t __List_ctor(List *const lst, const uint32_t elem_size)
+static  int32_t __List_ctor(List *const lst, const uint32_t elem_size)
 {
     if (lst == nullptr) return LIST_NULLPTR;
     if (lst->is_ctored) return INVALID_CTOR;
@@ -333,9 +351,8 @@ static uint32_t __List_ctor(List *const lst, const uint32_t elem_size)
 }
 #endif
 
-/* #define List_realloc(lst)                                                                                   \
-       _List_realloc(lst, __FILE__, __PRETTY_FUNCTION__, __LINE__)
-    
+/*_______________________________________________________________________________*/
+
 static int8_t  _List_realloc(List *const lst,   const char *call_file,
                                                 const char *call_func,
                                                 const char *call_line)
@@ -356,9 +373,59 @@ static int8_t __List_realloc(List *const lst)
             lst->data = calloc(sizeof(List_elem_info) + lst->elem_size, 4); //4 - elementary capacity
             if (lst->data == nullptr) return INVALID_MEMORY;
 
-            
+            lst->data_capacity = 4;
+            List_fill_free(lst);
+
+            #if HASH_DATA_PROTECTION
+            hash_culc(lst->data, lst->hash_data, lst->data_capacity);
         }
     }
 }
 
-static int8_t __List_fill_free(void *lst, ) */
+/*_______________________________________________________________________________*/
+
+static int8_t  _List_fill_free(List *const lst, const char *call_file,
+                                                const char *call_func,
+                                                const char *call_line)
+{
+    int8_t ret_val =    __List_fill_free(lst);
+    if    (ret_val == -1) log_place_dump()   ;
+    return ret_val;
+}
+
+static int8_t __List_fill_free(List *const lst)
+{
+    List_verify(lst);
+
+    lst->first_free  = lst->data_size;
+    int32_t elem_cnt = 0;
+
+    for (elem_cnt = lst->data_size; elem_cnt < lst->data_capacity - 1; ++elem_cnt)
+    {
+        void           * cur_elem =                     (int8_t *) lst->data + elem_cnt * (lst->elem_size + sizeof(List_elem_info));
+        List_elem_info * cur_info = (List_elem_info *) ((int8_t *) cur_elem  +             lst->elem_size                         );
+        List_elem_info *next_info = (List_elem_info *) ((int8_t *) cur_info  +             lst->elem_size + sizeof(List_elem_info));
+        cur_info->next            =                     (int8_t *) cur_info  +                               sizeof(List_elem_info);
+        
+        next_info->prev           = cur_elem;
+        cur_info ->is_free        = true;
+        
+        for (int32_t byte_cnt = 0; byte_cnt < lst->elem_size; ++byte_cnt) ((int8_t *) cur_elem)[byte_cnt] = POISON_ELEM_BYTE;
+    }
+
+    void           *last_elem  =                     (int8_t *) lst->data + elem_cnt * (lst->elem_size + sizeof(List_elem_info));
+    List_elem_info *last_info  = (List_elem_info *) ((int8_t *) last_elem +             lst->elem_size                         );
+    List_elem_info *first_info = (List_elem_info *) ((int8_t *) lst->data +             lst->elem_size                         );
+
+    last_info ->next           = lst->first_data;
+    first_info->prev           = last_elem;
+    last_info ->is_free        = true;
+
+    for (int32_t byte_cnt = 0; byte_cnt < lst->elem_size; ++byte_cnt) ((int8_t *) last_elem)[byte_cnt] = POISON_ELEM_BYTE;
+
+    List_verify(lst);
+
+    return OK;
+}
+
+/*_______________________________________________________________________________*/
